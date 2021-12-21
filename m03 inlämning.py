c2089d9e4af90a58b707d9d043785a3dab665952
Arien-S <92087056+Arien-S@users.noreply.github.com>
2021-12-21 09:54:57 +0100

    Add files via upload

--- m03 inlämning.py
+++ m03 inlämning.py
@@ -1,13 +1,15 @@
 
 salary = int(input("Hur mycket tjänar du i månaden i kr?"))
+
+#jag gör en input för variabeln salary och gör den till en intergear
+
 StateTax= 0
 CommunalTax = salary*0.2136
 CouncilTax = salary*0.1148
 SalaryNetto = salary - CommunalTax - CouncilTax
-TotalTax = CommunalTax + CouncilTax + StateTax
-TaxProcent = TotalTax/salary*100
-
 YearlySalary = 12*salary
+
+#Ger värden till alla mina variabler
 
 if YearlySalary < 19247:
     CommunalTax = 0
@@ -23,9 +25,13 @@ if YearlySalary > 675700:
     StateTax = salary*0.25
     SalaryNetto = salary - CommunalTax - CouncilTax - StateTax
     print(f"Din Statligaskatt är {round(StateTax)} kr")
+
+#skriver argument som sker beroende på angiven lön
     
-    
-    
+TotalTax = CommunalTax + CouncilTax + StateTax
+TaxProcent = TotalTax/salary*100
+
+#Den totala skatten räknas utifrån vilket argument som stämde in med den givna inputen
 
 print(f"Din lön är {round(salary)} kr i månaden")
 print(f"Din kommunalskatt är {round(CommunalTax)}  kr")
@@ -33,6 +39,8 @@ print(f"Din Landstingskatt är {round(CouncilTax)} kr")
 print(f"Din nettolön är {round(SalaryNetto)} kr")
 print(f"Din totala skatt är {round(TotalTax)} kr")
 print(f"Din totala procent skatt är {round(TaxProcent,2)}%")
+
+#printar alla beräkningar med hjälp av f-string samt avrundar de
 
 
 
